1fdd1b84d03fd5980f3002cbddbb84a032eaaf7f
jaroldsalmazan12 <jarold.salmazan@brightfin.com>
2024-05-10 19:37:23 +0800

    modify tf

--- us-dev/ec2/main.tf
+++ us-dev/ec2/main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "example" {
   instance_type          = "t2.micro"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
-
   tags = {
     Name = "tf-instance-cicd"
   }
