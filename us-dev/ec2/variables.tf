6b5072242ad108c145e5f172d46a064251f620dd
jaroldsalmazan12 <jarold.salmazan@brightfin.com>
2024-05-15 03:35:25 +0800

    deleteing ec2 provison via tf circleci

--- us-dev/ec2/variables.tf
+++ us-dev/ec2/variables.tf
@@ -1,11 +1,11 @@
-# variable "subnet_id" {
-#   description = "The subnet ID where the EC2 instance will be launched"
-# }
+variable "subnet_id" {
+  description = "The subnet ID where the EC2 instance will be launched"
+}
 
-# variable "security_group_id" {
-#   description = "The ID of the existing security group to attach to the EC2 instance"
-# }
+variable "security_group_id" {
+  description = "The ID of the existing security group to attach to the EC2 instance"
+}
 
-# variable "ami_id" {
-#   description = "The AMI ID for the EC2 instance"
-# }
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+}
